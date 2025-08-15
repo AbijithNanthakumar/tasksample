f7435fc7ac6d5102a2138a60afef9ab6359ad220
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-15 20:37:52 +0530

    process change

--- DoctorPatient/DoctorPatient.cpp
+++ DoctorPatient/DoctorPatient.cpp
@@ -2,6 +2,27 @@
 #include <cctype>
 using namespace std;
 
-int main(){
+string toLowercase(string str){
+    for(char &ch : str){
+        ch = tolower(ch);
+    }
+    return str;
+}
+
+
+void process(){
+    string isemergency;
+    cout << "Is that a emeergency ?";
+    getline(cin,isemergency);
+
+    if(toLowercase(isemergency) == "yes" || toLowercase(isemergency) == "y"){
+        DoctorAvailablity();
+    }
     
+}
+
+int main(){
+    cout << "Patient Arrives";
+    
+    process();
 }
