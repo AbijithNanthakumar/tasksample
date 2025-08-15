3e5de768a983d04082015e77dab80d061a6e32bd
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-15 19:19:48 +0530

    start

--- RainingCode/raining.cpp
+++ RainingCode/raining.cpp
@@ -2,8 +2,25 @@
 #include <string>
 using namespace std;
 
+string toLowercase(string str){
+   for(char &ch : str){
+      ch = tolower(ch);
+   }
+
+   cout << str;
+
+   return str;
+}
+
 int main(){
-    
+    string israining;
+    cin >> israining;
+    getline(cin, israining);
+
+    string lower = toLowercase(israining);
+    cout << lower;
+
+
     
     
 }
