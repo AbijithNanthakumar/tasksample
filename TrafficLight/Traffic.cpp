62a2ee609c3ad9df945c08925826d8c6b39d6244
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-16 09:03:16 +0530

    all chnges late commit

--- TrafficLight/Traffic.cpp
+++ TrafficLight/Traffic.cpp
@@ -28,6 +28,59 @@ string toLowercase(string str)
     return str;
 }
 
+void Directions(){
+    cout << "Look Left";
+    string okleft;
+    cin >> okleft;
+
+       if (!isformat(okleft))
+        {
+            cout << "Enter the proper input y or n";
+        }
+        else if (toLowercase(okleft) == "yes" || toLowercase(okleft) == "y")
+        {
+            Directions();
+        }
+        else{
+            cout << "Look right";
+        }
+
+}
+
+void Color(){
+    cout << "what is the color";
+         string color;
+         cin >> color;
+
+        if (!isformat(color))
+        {
+            cout << "Enter the proper input y or n";
+        }
+        else if (toLowercase(color) == "red" || toLowercase(color) == "r")
+        {
+            Directions();
+        }
+        else
+        {
+           this_thread::sleep_for(chrono::seconds(3)); 
+           Color();
+        }
+}
+void TrafficLight(){
+    string trafficlight;
+    cout << "Is traffic light present ?";
+    cin >> trafficlight;
+
+    if(!isformat(trafficlight)){
+        cout << "Enter proper input y or n";
+    }
+    else if(toLowercase(trafficlight)=="yes" ||  toLowercase(trafficlight)=="y"){
+         Color();
+    }
+    else{
+        Directions();
+    }
+}
 int main()
 {
     while (true)
@@ -36,7 +89,7 @@ int main()
         cout << "Is footbridge avaible ?";
         cin >> isfootbridge;
 
-        if (!isformat)
+        if (!isformat(isfootbridge))
         {
             cout << "Enter the proper input y or n";
         }
@@ -47,18 +100,35 @@ int main()
         }
         else
         {
-            string isfootbridge;
-            cout << "Is footbridge avaible ?";
-            cin >> isfootbridge;
+            string istunnel;
+            cout << "Is tunnel avaible ?";
+            cin >> istunnel;
 
-            if (!isformat)
+            if (!isformat(istunnel))
             {
                 cout << "Enter the proper input y or n";
             }
-            else if (toLowercase(isfootbridge) == "yes" || toLowercase(isfootbridge) == "y")
+            else if (toLowercase(istunnel) == "yes" || toLowercase(istunnel) == "y")
             {
-                cout << "Use footbridge";
+                cout << "Use Crossing";
                 return 0;
+            }
+            else{
+              
+            string iscrossing;
+            cout << "Is crossing avaible ?";
+            cin >> iscrossing;
+
+            if (!isformat(iscrossing))
+            {
+                cout << "Enter the proper input y or n";
+            }
+            if(toLowercase(iscrossing) == "yes" || toLowercase(iscrossing) == "y")
+            {
+                cout << "Use crossing";
+                TrafficLight();
+            }
+              
             }
         }
     }
