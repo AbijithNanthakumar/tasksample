eb238587d5ae0a1652860dc02c1d8f2b0541760c
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-16 08:06:55 +0530

    new changes made

--- TrafficLight/Traffic.cpp
+++ TrafficLight/Traffic.cpp
@@ -4,6 +4,62 @@
 #include <thread>
 using namespace std;
 
-int main(){
-    
+// check
+bool isformat(string str)
+{
+    for (char ch : str)
+    {
+        if (!isalpha(ch) && !isspace(ch))
+        {
+            return true;
+        }
+    }
+    return true;
+}
+
+// toLowercase
+string toLowercase(string str)
+{
+    for (char &ch : str)
+    {
+        ch = tolower(ch);
+    }
+
+    return str;
+}
+
+int main()
+{
+    while (true)
+    {
+        string isfootbridge;
+        cout << "Is footbridge avaible ?";
+        cin >> isfootbridge;
+
+        if (!isformat)
+        {
+            cout << "Enter the proper input y or n";
+        }
+        else if (toLowercase(isfootbridge) == "yes" || toLowercase(isfootbridge) == "y")
+        {
+            cout << "Use footbridge";
+            return 0;
+        }
+        else
+        {
+            string isfootbridge;
+            cout << "Is footbridge avaible ?";
+            cin >> isfootbridge;
+
+            if (!isformat)
+            {
+                cout << "Enter the proper input y or n";
+            }
+            else if (toLowercase(isfootbridge) == "yes" || toLowercase(isfootbridge) == "y")
+            {
+                cout << "Use footbridge";
+                return 0;
+            }
+        }
+    }
 }
