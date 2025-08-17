18ba271781af39980f2b1bfc5659562d50538b3f
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-17 09:52:05 +0530

    static value completed

--- OnlineShopping/onlineshopping.cpp
+++ OnlineShopping/onlineshopping.cpp
@@ -3,11 +3,39 @@
 #include <string>
 using namespace std;
 
-
 struct Item {
     string name;
     bool inWarehouse;
 };
+void warehousetocustomer(){
+    cout << "Sending goods from warehouse to the customer \n";
+    cout << "Customer recieved the product \n";
+}
+void sellershipping(){
+    cout << "Seller shipped the item to the customer \n";
+}
+
+void contactseller(const string &name){
+    cout << "Contacting seller to ship" << name << "to customer \n";
+}
+
+void Warehousepacking(const string &name){
+     cout << name << "packed with other ordders \n";
+}
+
+
+void processing(vector<Item>& items){
+     for(const auto& item :items){
+         if(item.inWarehouse){
+             Warehousepacking(item.name);
+         }
+         else{
+            contactseller(item.name);
+            sellershipping();
+         }
+     }
+}
+
 
 void Order(vector<Item>& items) {
     cout << "There are" << items.size() << " items ordered.\n";
@@ -22,6 +50,10 @@ int main() {
     };
 
     Order(items);
+    processing(items);
+    warehousetocustomer();
+
+    cout << "Completed \n";
    
 }
 
