bfa14bed0cce387fadccbdb974eef3f9024803c3
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-17 09:29:27 +0530

    new concept struct

---
+++ OnlineShopping/onlineshopping.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+struct Item {
+    string name;
+    bool inWarehouse;
+};
+
+int main() {
+    vector<Item> items = {
+        {"Sketcbook", true},
+        {"pencil", true},
+        {"stensil", false},
+        {"tablet", true}
+    };
+
+   
+}
+
+
+
