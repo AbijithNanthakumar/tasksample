c7ed03033113f23250feec601e443199e22d26fb
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-17 10:39:38 +0530

    new folder Trainbook

---
+++ TainBooking/TrainBook.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool isFormat(char ch)
+{
+        if (!isalpha(ch) && !isspace(ch))
+        {
+            return false;
+        }
+    
+    return true;
+}
+
+void Reservation(){
+    
+}
+
+int main(){
+    cout << "But ticket for train";
+    
+    Reservation();
+
+
+
+}
