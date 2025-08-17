2df26b4540387c5abd8543f61713cc1b7ebca343
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-17 08:37:01 +0530

    Completed with delay 5 seconds

--- BoardingPlane/BoardingPlane.cpp
+++ BoardingPlane/BoardingPlane.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cctype>
 #include <string>
+#include <thread>
 using namespace std;
 
 void PriorityBoard();
@@ -26,7 +27,27 @@ string toLowercase(string str)
 }
 
 void Announcement(){
+    cout << " Board withn 5 minutes \n";
+    string isarrived;
+    getline(cin, isarrived);
+
+    if(!isFormat(isarrived)){
+        cout << "Enter the proper input y or n \n";
     
+    }
+    if(toLowercase(isarrived)=="yes" || toLowercase(isarrived)=="y"){
+        cout << "All passengers arrived.Closing Gate \n";
+        return;
+    }
+    else{
+       cout << "Passengers are requested to onboard. \n";
+        this_thread::sleep_for(chrono::seconds(5));
+        cout << "Closing gate \n";
+        return;
+    }
+
+
+
 }
 
 void Halfclass(){
@@ -35,12 +56,12 @@ void Halfclass(){
     getline(cin, isHalffirstclass);
 
     if(!isFormat(isHalffirstclass)){
-        cout << "Enter the proper input y or n";
+        cout << "Enter the proper input y or n \n";
         Halfclass();
     }
     if(toLowercase(isHalffirstclass)=="yes" || toLowercase(isHalffirstclass)=="y"){
-        cout << "All Passengers Board Plane";
-         Annoucement();
+        cout << "All Passengers Board Plane \n";
+         Announcement();
     }
     else{
      PriorityBoard();
@@ -55,12 +76,12 @@ void PriorityBoard(){
     getline(cin, isfirstclass);
 
     if(!isFormat(isfirstclass)){
-        cout << "Enter the proper input y or n";
+        cout << "Enter the proper input y or n \n";
         PriorityBoard();
     }
     if(toLowercase(isfirstclass)=="yes" || toLowercase(isfirstclass)=="y"){
-        cout << "Board Plane";
-        // Annoucement();
+        cout << "Board Plane \n";
+         Announcement();
     }
     else{
        Halfclass();
