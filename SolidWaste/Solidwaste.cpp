f155207d798d2a5a55c53f2a8d36483f8151c6bb
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-17 06:58:51 +0530

    final changes made

--- SolidWaste/Solidwaste.cpp
+++ SolidWaste/Solidwaste.cpp
@@ -18,30 +18,82 @@ string toLowercase(string str){
     return str;
 }
 
+void Recyclable(){
+     cout << "Is this Recyclable? \n";
+    string isrecycle;
+    getline(cin,isrecycle);
+
+    if(!isFormat(isrecycle)){
+        cout << "Enter proper input y or n \n";
+        Recyclable();
+    }
+    if(toLowercase(isrecycle)=="yes" || toLowercase(isrecycle)=="y"){
+        cout << "This is a RECYCLABLE waste, can be reused for other applications. \n";
+        return;
+    }
+    else{
+        cout << "This is not Recyclable also and can be dumbed in LAND FILL. \n";
+        
+    }
+}
+void Reuseable(){
+    cout << "Is this Reusable? \n";
+    string isreuse;
+    getline(cin,isreuse);
+
+    if(!isFormat(isreuse)){
+        cout << "Enter proper input y or n \n";
+        Reuseable();
+    }
+    if(toLowercase(isreuse)=="yes" || toLowercase(isreuse)=="y"){
+        cout << "This is a reusable waste, can be reused for other applications. \n";
+        return;
+    }
+    else{
+        cout << "This is not Reusable. \n";
+        Recyclable();
+    }
+}
 void HighEnergy(){
-    string 
+    cout << "Is this high Energy ? \n";
+    string isHigh;
+    getline(cin,isHigh);
+
+    if(!isFormat(isHigh)){
+        cout << "Enter proper input y or n \n";
+        HighEnergy();
+    }
+    if(toLowercase(isHigh)=="yes" || toLowercase(isHigh)=="y"){
+        cout << "This is a High Energy waste used as BIO-FUEL. \n";
+        return;
+    }
+    else{
+        cout << "This is not a High Energy waste, can be used as ORGANIC FERTILIZER. \n";
+        return;
+    }
 }
 
 void WasteProcess(){
     cout << "Is this a organic waste? \n";
     string isorganic;
-    cin >> isorganic;
+    getline(cin,isorganic);
 
     if(!isFormat(isorganic)){
         cout << "Enter the proper input y or n \n";
+        WasteProcess();
     }
     if(toLowercase(isorganic) == "yes" || toLowercase(isorganic) == "y"){
-        cout << "This is a organic waste. Is this a High energy waste? \n";
+        cout << "This is a organic waste. \n";
         HighEnergy();
     }
     else{
-        cout << "This is not a organic waste. Is this Reuseable? \n"
+        cout << "This is not a organic waste. \n";
         Reuseable();
     }
 }
 
 int main(){
-    cout << "Getting Waste";
+    cout << "Getting Waste \n";
      
     WasteProcess();
 
