70a31133eaf1d78304de2af313354475b8f0a8b8
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-17 17:56:54 +0530

    process method written and final commit

--- FireCode/Fire.cpp
+++ FireCode/Fire.cpp
@@ -52,3 +52,59 @@ void Inside() {
     cout << "Wave at the window to identify survivors.\n";
     cout << "Wait for rescue.\n";
 }
+void process() {
+    
+    cout << "Bring key, wallet, phone, and wet towel.\n";
+    cout << "Is the door hot or smoke coming? (yes=1 / no=0) ";
+    int hotDoor;
+    cin >> hotDoor;
+
+    if (hotDoor == 1) {
+        Inside(); 
+    } 
+    else {
+        cout << "Go downstairs until you leave the building.\n";
+        cout << "Did you escape the building? (yes=1 / no=0): ";
+        int escaped;
+        cin >> escaped;
+
+        if (escaped == 1) {
+            cout << " You are safe outside!\n";
+        } 
+        else {
+        while(true){
+            cout << "Do you see fire/strong smoke? (yes=1 / no=0): ";
+            int smoke;
+            cin >> smoke;
+
+            if (smoke == 1) {
+                cout << "Go upstairs and head to the balcony.\n";
+                cout << "Did you reach the balcony? (yes=1 / no=0): ";
+                int balcony;
+                cin >> balcony;
+
+                if (balcony == 1) {
+                    cout << "Wait for rescue at the balcony.\n";
+                    break;
+                } else {
+                    cout << "Do you see fire/strong smoke? (yes=1 / no=0): ";
+                    int s;
+                    cin >> s;
+                    if(s == 1){
+                       cout << "Find and Enter a safe room \n";
+                       Inside();
+                    }
+                    else{
+                        continue;
+                    }
+                }
+            } 
+            else {
+                cout << "Find a room and enter safe.\n";
+            }
+        } 
+
+        }
+    }
+   
+}
