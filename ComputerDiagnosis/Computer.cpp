e587264f3ee1e8fc0cd7f977deaaf567b9a48b94
AbijithNanthakumar <abijith.nanthakumar@psiog.com>
2025-08-17 07:29:09 +0530

    final changes made and commit

--- ComputerDiagnosis/Computer.cpp
+++ ComputerDiagnosis/Computer.cpp
@@ -3,7 +3,117 @@
 #include <string>
 using namespace std;
 
-int main(){
-    cout << "Is computer on? \n";
+void check();
 
+bool isFormat(string str)
+{
+    for (char ch : str)
+    {
+        if (!isalpha(ch) && !isspace(ch))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+string toLowercase(string str)
+{
+    for (char &ch : str)
+    {
+        ch = tolower(ch);
+    }
+    return str;
+}
+
+void ErrorProcess()
+{
+    cout << "Is the any error message displayed? \n";
+    string iserr;
+    cin >> iserr;
+
+    if (!isFormat(iserr))
+    {
+        cout << "Enter the proper input y or n. \n";
+        ErrorProcess();
+    }
+    if (toLowercase(iserr) == "yes" || toLowercase(iserr) == "y")
+    {
+        cout << "Perform Diagnosis for the computer. \n";
+        check();
+    }
+    else
+    {
+        cout << "Computer is in good condition. \n";
+        return;
+    }
+}
+
+void Plugged(){
+     cout << "Is the power plugged to the wall?. \n";
+    string isplugged;
+    cin >> isplugged;
+        if (!isFormat(isplugged))
+        {
+            cout << "Enter the proper input y or n. \n";
+            Powerprocess();
+        }
+        if (toLowercase(isplugged) == "yes" || toLowercase(isplugged) == "y")
+        {
+            cout << "Find a specialist to repair. \n";
+            return;
+        }
+        else
+        {
+            cout << "Plug power to the wall. \n"
+        }
+}
+
+void Powerprocess()
+{
+    cout << "Is the power light on.? \n";
+    string ispower;
+    cin >> ispower;
+
+    if (!isFormat(ispower))
+    {
+        cout << "Enter the proper input y or n. \n";
+        Powerprocess();
+    }
+    if (toLowercase(ispower) == "yes" || toLowercase(ispower) == "y")
+    {
+        cout << "Find a specialist to repair. \n";
+        return;
+    }
+    else
+    {
+        Plugged();
+        
+    }
+}
+
+void check()
+{
+    cout << "Is computer on? \n";
+    string ison;
+    cin >> ison;
+
+    if (!isFormat(ison))
+    {
+        cout << "Enter the proper input y or n. \n";
+        check();
+    }
+    if (toLowercase(ison) == "yes" || toLowercase(ison) == "y")
+    {
+        ErrorProcess();
+    }
+    else
+    {
+        Powerprocess();
+    }
+}
+
+int main()
+{
+
+    check();
 }
